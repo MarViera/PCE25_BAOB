3338ef1bd13fedc966ec09416cbca9ba0fdf1f17
Briseidy Oranday <briseidy.orandaybrn@uanl.edu.mx>
2025-05-23 17:28:03 -0600

    Update 3erP_PCE25.py

--- 3erP_PCE25.py
+++ 3erP_PCE25.py
@@ -1,16 +1,16 @@
 import requests
 import json
 #3er Parcial PC
-def checarIP():
+def checarIP(ip, apikey):
     #Código para consultar la API de Abuse IP BD
     url = 'https://api.abuseipdb.com/api/v2/check'
     querystring = {
-        'ipAddress': '52.123.250.32',
+        'ipAddress': ip,
         'maxAgeInDays': '90'
     }
     headers = {
         'Accept': 'application/json',
-        'Key': '1351d01769557d0f4f0e59265e9b3b401a122bac5c33b6fcbf15ac6f7a1d7e55b5d53ff5f43a287c'
+        'Key': apikey
     }
     response = requests.request(method='GET', url=url,
                                 headers=headers, params=querystring)
@@ -18,11 +18,44 @@ def checarIP():
     decodedResponse = json.loads(response.text)
     return json.dumps(decodedResponse, sort_keys=True, indent=4)
 
-def main():
-    #todo el codigo del proyecto estará aquí
-    print("Hola mundo")
-    print(checarIP())
+#Analisis de la API
 
+    if response.status_code == 200:
+        score = data['abuseConfidenceScore']
+        domain = data.get('domain', 'Dominio no encontrado')
+        country = data.get('countryCode', 'Desconocido')
+        total_reports = data['totalReports']
+   
+#Verificar la actividad de la IP para corroborar si es peligrosa o no
+
+    if score == 0:
+        return['La IP {ip} no tiene reportes de abuso y se considera segura']
+    elif score < 50:
+        return ['La IP {ip} tiene algunos reportes ({totalReports}), pero no se considera maliciosa']
+    else:
+        return['La IP {ip} tiene un alto nivel de reportes ({totalReports}) y se considera maliciosa']
+
+def main():
+    apikey = 'e91328a52e454203e996f806aa803356bd725d7e35fce8424c9a0439c853fa4c94ec4c0d26c33b22'
+    lista_ips = ['52.123.250.32', '103.172.204.220', '69.98.208.120', '185.93.89.118']
+
+    print("Verificando solo las primeras 3 IPs por límite de la API:\n")
+
+    for ip in lista_ips[:3]:
+        resultado = checarIP(ip, apikey) 
+        print(resultado)
+        print("=" * 50)
 
 if __name__ == "__main__":
     main()
+
+
+ 
+
+
+
+
+
+
+
+
